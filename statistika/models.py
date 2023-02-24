09c4639e3483d2e41c47d371a555fa324172faf4
dornadoff <dornadoff@gmail.com>
2023-02-24 16:47:53 +0500

    Omborxona loyiha

--- statistika/models.py
+++ statistika/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ombor.models import Client, Mahsulot, Ombor
 
-# Create your models here.
+class Statistika(models.Model):
+    mahsulot = models.ForeignKey(Mahsulot, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    miqdor = models.PositiveIntegerField()
+    sana = models.DateTimeField(auto_now_add=True)
+    umumiy_summa = models.PositiveBigIntegerField()
+    tolandi = models.PositiveBigIntegerField()
+    nasiya =  models.PositiveBigIntegerField()
